5fab1c04759f85f0847ede4b7c2c82d332b0d6d9
algmoreno <alg.moreno00@gmail.com>
2021-09-28 13:06:43 -0700

    featured product card

--- client/src/components/HomeCard/index.js
+++ client/src/components/HomeCard/index.js
@@ -2,22 +2,30 @@ import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card'
-import cardImage from '../../assets/images/dogandowner.jpg';
+import CardGroup from 'react-bootstrap/CardGroup';
+import homeImage from '../../assets/images/dogandowner.jpg';
 
 function HomeCard() {
   return (
-    <Card className="bg-dark text-white">
-    <Card.Img src={cardImage} alt="Card image" />
-    <Card.ImgOverlay>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-      <Card.Text>Last updated 3 mins ago</Card.Text>
-    </Card.ImgOverlay>
-  </Card>
+    <Card className="bg-light transparent text-white">
+      <Card.Img src={homeImage} alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title className="main-card-head">Welcome to The Pet Outlet!</Card.Title>
+        <Card.Text className="feat-card-text">Featured</Card.Text>
+        <Card className="bg-dark feat-card text-white">
+          <Card.Img src="holder.js/100px270" alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+            <Card.Text>Last updated 3 mins ago</Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+      </Card.ImgOverlay>
+    </Card>
   )
 }
 
-export default HomeCard; 
+export default HomeCard;
