be63545607fa29e1940c0acd5a0d2f02666069c0
bobswiftfoot <natepfau@yahoo.com>
2021-09-28 20:44:27 -0500

    Added Product model/schema

---
+++ server/models/Product.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+{
+    name:
+    {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description:
+    {
+        type: String
+    },
+    image:
+    {
+        type: String
+    },
+    price:
+    {
+        type: Number,
+        required: true,
+        min: 0.99
+    },
+    quantity:
+    {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    category:
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    //reviews: [Review],
+    rating: 
+    {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    featuredProduct:
+    {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -17,17 +17,37 @@ const typeDefs = gql`
     subcategories: [Category]
   }
 
+  type Product
+  {
+      _id: ID
+      name: String
+      description: String
+      price: Float
+      category: Category
+      rating: Float
+      featuredProduct: Boolean
+  }
+
   type Query 
   {
-    user(_id: ID!): User
     users: [User]
+    user(_id: ID!): User
     categories: [Category]
-    subcategories(_id: ID): [Category]
+    subcategories(_id: ID!): [Category]
+    products(category: ID): [Product]
+    product(_id: ID!): Product
+    featuredProducts(category: ID!): [Product]
   }
 
   type Mutation 
   {
     addUser(firstName: String!, lastName: String!, userName: String!, email: String!, password: String!): User
+    addCategory(name: String!, subcategories: [ID]): Category
+    editCategory(_id: ID!, name: String, subcategories: [ID]): Category
+    removeCategory(_id: ID!): Category
+    addProduct(name: String!, description: String, price: Float!, category: ID!, featuredProduct: Boolean): Product
+    editProduct(_id: ID!, name: String, description: String, price: Float, category: ID, featuredProduct: Boolean, rating: Float): Product
+    removeProduct(_id: ID!): Product
   }
 `;
 
