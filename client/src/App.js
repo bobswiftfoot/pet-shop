f078580a462fd47308284ef6c5103d7c658f5195
bobswiftfoot <natepfau@yahoo.com>
2021-09-30 21:00:37 -0500

    Adding empty Admin page and adding to App Routes

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Admin from './pages/Admin';
 import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
@@ -51,6 +52,7 @@ function App() {
               <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/admin" component={Admin} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
