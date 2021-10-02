bfb7aac1607016d40d38b1de97009e341053745a
bobswiftfoot <natepfau@yahoo.com>
2021-10-01 19:54:57 -0500

    Show Categories

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from '@apollo/client';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import { QUERY_ALL_CATEGORIES } from '../utils/queries';
 
-const Admin = () => {
-  return (
-    <div>
+const Admin = () =>
+{
+    const [showCategories, setshowCategories] = useState(false);
 
-    </div>
-  );
+    const { loading: loadingCategories, data: categoryData } = useQuery(QUERY_ALL_CATEGORIES);
+
+
+    function toggleCategories()
+    {
+        setshowCategories(!showCategories);
+        console.log(categoryData);
+    };
+
+    return (
+        <div>
+            <Button variant="primary" onClick={toggleCategories}>Categories</Button>
+            {!loadingCategories && showCategories ? (
+                <ListGroup>
+                    {categoryData.categories.map((category) => (
+                        <ListGroup.Item key={category.name}>
+                            <InputGroup>
+                                <InputGroup.Text>Name:</InputGroup.Text>
+                                <FormControl type="text" placeholder="Name" defaultValue={category.name}></FormControl>
+                                
+                            </InputGroup>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            ) : null}
+        </div>
+    );
 };
 
 export default Admin;
