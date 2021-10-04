6432e1ca90e44a0e03548feeecf8519b7b20f9e4
bobswiftfoot <natepfau@yahoo.com>
2021-10-03 23:36:34 -0500

    Merge Conflicts

--- client/src/components/AdminProduct/index.js
+++ client/src/components/AdminProduct/index.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 import React from 'react';
 import { useMutation } from '@apollo/client';
 import { ListGroup, InputGroup, FormControl, Form, Button, Row, Col, Container } from 'react-bootstrap';
 import { EDIT_PRODUCT, REMOVE_PRODUCT } from '../../utils/mutations';
 import { calculateRating } from '../../utils/helpers'
-=======
-import React from "react";
-import { useMutation } from '@apollo/client';
-import { ListGroup, InputGroup, FormControl, Form, Button } from 'react-bootstrap';
-import { EDIT_PRODUCT } from '../../utils/mutations';
->>>>>>> alan
 
 const AdminProduct = (props) =>
 {
     const [editProduct] = useMutation(EDIT_PRODUCT);
-<<<<<<< HEAD
     const [removeProduct] = useMutation(REMOVE_PRODUCT);
 
     const handleSaveProduct = async (event) =>
@@ -25,31 +17,15 @@ const AdminProduct = (props) =>
         const priceValue = parseFloat(event.target.parentNode.children[3].children[1].children[0].value);
         const { id: categoryValue } = event.target.parentNode.children[5].children[1].children[0].selectedOptions[0];
         const featuredValue = event.target.parentNode.children[6].children[1].children[0].value === 'true';
-=======
-
-    const handleSaveProduct = async (event) =>
-    {
-        const { value: nameValue, id } = event.target.parentNode.children[0].children[1];
-        const { value: descriptionValue } = event.target.parentNode.children[1].children[1];
-        const { value: imageValue } = event.target.parentNode.children[2].children[1];
-        const priceValue = parseFloat(event.target.parentNode.children[3].children[1].value);
-        const { id: categoryValue } = event.target.parentNode.children[4].children[1].selectedOptions[0];
-        const featuredValue = event.target.parentNode.children[5].children[1].checked;
->>>>>>> alan
 
         try
         {
             await editProduct({ variables: { editProductId: id, editProductName: nameValue, editProductDescription: descriptionValue, editProductImage: imageValue,  editProductPrice: priceValue,editProductCategory: categoryValue,editProductFeaturedProduct: featuredValue } });
-<<<<<<< HEAD
-=======
-            window.location.reload();
->>>>>>> alan
         }
         catch (e)   
         {
             console.log(e);
         }
-<<<<<<< HEAD
     };    
 
     const handleDeleteProduct = async (event) =>
@@ -119,50 +95,6 @@ const AdminProduct = (props) =>
                 ))}
             </ListGroup>
         </Container>
-=======
-    };
-
-    return (
-        <ListGroup>
-            {props.productData.products.map((product) => (
-                <ListGroup.Item key={product._id}>
-                    <InputGroup>
-                        <InputGroup.Text>Name:</InputGroup.Text>
-                        <FormControl type="text" placeholder="Name" id={product._id} defaultValue={product.name}></FormControl>
-                    </InputGroup>
-                    <InputGroup>
-                        <InputGroup.Text>Description:</InputGroup.Text>
-                        <FormControl type="text" placeholder="Description" defaultValue={product.description}></FormControl>
-                    </InputGroup>
-                    <InputGroup>
-                        <InputGroup.Text>Image:</InputGroup.Text>
-                        {/* TODO: Change this to a drop down containing all image chioces*/}
-                        <FormControl type="text" placeholder="Image" defaultValue={product.image}></FormControl>
-                    </InputGroup>
-                    <InputGroup>
-                        <InputGroup.Text>Price:</InputGroup.Text>
-                        <FormControl type="text" placeholder="Price" defaultValue={product.price}></FormControl>
-                    </InputGroup>
-                    <InputGroup>
-                        <InputGroup.Text>Category:</InputGroup.Text>
-                        <Form.Select key={product.category._id}>
-                            {props.categoryData.categories.map(listCategory => (
-                                listCategory._id === product.category._id) ?
-                                <option key={listCategory._id} id={listCategory._id} selected>{listCategory.name}</option> :
-                                <option key={listCategory._id} id={listCategory._id}>{listCategory.name}</option>
-                            )}
-                        </Form.Select>
-                    </InputGroup>
-                    <InputGroup>
-                        <InputGroup.Text>Featured:</InputGroup.Text>
-                        {/* Using createElement to make use of defaultChecked */}
-                        {React.createElement('input', { type: 'checkbox', defaultChecked: product.featuredProduct })}
-                    </InputGroup>
-                    <Button type="submit" onClick={handleSaveProduct}>Save Product</Button>
-                </ListGroup.Item>
-            ))}
-        </ListGroup>
->>>>>>> alan
     );
 };
 
